d1d1b6de0b80764a1018caa987b9dbbfcb30a807
andrewakosta <47621944+andrewakosta@users.noreply.github.com>
2020-07-16 15:47:44 -0500

    Docuementation of code added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const listReminders = document.getElementById('list-reminders')
 const form = document.getElementById('form')
 const checkboxs = document.getElementById('checkboxs-content')
 let   checkValue;
+
 const checkboxsGroup = [
   document.getElementById('low'),
   document.getElementById('median'),
@@ -10,7 +11,9 @@ const checkboxsGroup = [
 
 eventListeners()
 
-
+/**
+*It loads the evnts as soons as the page is loads
+*/
 function eventListeners(){
 
   form.addEventListener('submit',addReminder)
@@ -20,8 +23,10 @@ function eventListeners(){
 
 }
 
+/**
+*It adds on the local storage the reminder and also call the method to add on DOM
+*/
 function addReminder(e){
-
   e.preventDefault()
   const reminder = document.getElementById('reminder')
   getValueFromCheckbox()
@@ -30,8 +35,10 @@ function addReminder(e){
   loadReminders()
 }
 
-
-//FUnction to fill space white########################################################################
+/**
+*It removes a reminder from DOM and it also call the methos to remove 
+* from local storage
+*/
 function removeReminder(e){
   e.preventDefault()
   if(e.target.className === 'remove-reminder'){
@@ -39,6 +46,9 @@ function removeReminder(e){
     removeRemainderOnLocalStorage(e.target.parentElement.innerText)
   }
 }
+/**
+*This is a main method which take all reminder from local atorage and send them to DOM
+*/
 function loadReminders(){
   let reminders = getRemindersFromlocalStorage()
   listReminders.innerText = ''
@@ -58,13 +68,20 @@ function loadReminders(){
 }
 
 
-
+/**
+*Add a reminder in the local storage, first it is add to array
+* then the array  is converted to string, finally teh string is added 
+*local storage
+*/
 function addReminderToLocalStorage(reminder){
   let reminders = getRemindersFromlocalStorage()
   reminders.push(reminder)
   localStorage.setItem('reminders',JSON.stringify(reminders))
 }
-
+/**
+*Retursn a array with the reminders on the local storage, in the case that it is
+* empty it returns the array empty
+*/
 function getRemindersFromlocalStorage(){
   let reminders
   if(localStorage.getItem('reminders')=== null){
@@ -75,7 +92,10 @@ function getRemindersFromlocalStorage(){
 
   return orderReminders(reminders)
 }
-
+/**
+*Remove a reminder from local storage throught its content,
+*if the content is the same in the DOM and local storage it is removed
+*/
 function removeRemainderOnLocalStorage(reminder){
   let reminders, removeReminder
   removeRminder = reminder.substring(0,reminder.length -1)
@@ -90,13 +110,18 @@ function removeRemainderOnLocalStorage(reminder){
   localStorage.setItem('reminders',JSON.stringify(reminders))
 }
 
+/**
+*Check that in the form only is one checkbox selected
+*/
 function checkboxsValidate(e){
   checkboxsGroup.forEach((checkbox)=>{
     checkbox.checked = 0
   })
   e.target.checked = 1
 }
-
+/**
+*Get the value from the form to the checkbox of imporatance of reminder
+*/
 function getValueFromCheckbox(){
   checkboxsGroup.forEach((checkbox)=>{
   if(checkbox.checked){
@@ -104,7 +129,9 @@ function getValueFromCheckbox(){
   }
 })
 }
-
+/**
+*It takes all reminders form local storage and returns them sorted in order of importance
+*/
 function orderReminders(reminders){
   const remindersOrdered = []
   reminders.forEach((reminder)=>{
@@ -120,6 +147,5 @@ function orderReminders(reminders){
     if(reminder[reminder.length -1] === '3'){
     remindersOrdered.push(reminder)
   }})
- console.log(remindersOrdered)
   return remindersOrdered
 }
